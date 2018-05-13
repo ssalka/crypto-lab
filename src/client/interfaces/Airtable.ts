56d5ede0794447ccdcf0c1e99509894efea72f2a
Steven Salka <stevensalka925@gmail.com>
2018-05-13 19:22:02 -0400

    add missing airtable fields

--- src/client/interfaces/Airtable.ts
+++ src/client/interfaces/Airtable.ts
@@ -5,14 +5,32 @@ interface IAirtableLogo {
   url: string;
 }
 
+interface IAirtableThumbnail {
+  url: string;
+  width: number;
+  height: number;
+}
+
+interface IAirtableAttachment {
+  filename: string;
+  id: string;
+  size: number;
+  thumbnails: Record<'large' | 'small', IAirtableThumbnail>;
+  type: string;
+  url: string;
+}
+
 export interface IAirtableCoin {
   Blockchain: string[];
   Bookmarks: string[];
   'Buy Target': string;
   Category: ProjectCategory;
+  'Consensus Mechanism': string;
+  Contributors: string[];
   'Date Added': string;
   Favorites: boolean;
   Features: string[];
+  'Fork(s)': string[];
   HODL: boolean;
   Industry: string;
   Joined: boolean;
@@ -22,6 +40,7 @@ export interface IAirtableCoin {
   Logo: IAirtableLogo[];
   'Market Cap': string;
   Name: ProjectName;
+  'Not Getting Any Time Soon': boolean;
   Notes: string;
   'Official Website': string;
   'Potential Project Element': boolean;
@@ -29,14 +48,13 @@ export interface IAirtableCoin {
   Rank: number;
   'Reasons to Like': string[];
   'Related Actors': string[];
+  'Second Top 5': boolean;
   Stage: string;
   Symbol: CurrencyCode;
+  Tags: string[];
   'Top 5': boolean;
+  'Trading Notes': string[];
   Type: ProjectType;
+  'Whitepaper(s)': IAirtableAttachment[];
+  'Written In': string[];
 }
-
-// export interface ICoinMarketCapSchema {
-//   marketCap: number;
-//   price: number;
-// }
-//
