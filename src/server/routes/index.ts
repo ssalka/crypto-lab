0409f9085286057b703f7040ccc81bb58c5aecec
Steven Salka <stevensalka925@gmail.com>
2018-05-20 14:16:34 -0400

    load more pages from CoinMarketCap

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,21 +1,28 @@
 import CoinMarketCap from 'coinmarketcap-api';
-import express from 'express';
-import _ from 'lodash';
+import { Router } from 'express';
+import _ from 'lodash/fp';
 
 import { IAirtableCoin } from 'src/client/interfaces/Airtable';
-import { ICoinMarketCapResponse } from 'src/client/interfaces/CoinMarketCap';
+import { ICoinMarketCapCoin, ICoinMarketCapResponse } from 'src/client/interfaces/CoinMarketCap';
 import { indexHtml } from '../config';
 import { getCoins } from './airtable';
 
 const cmc = new CoinMarketCap();
 
-const router = express.Router();
+const router = Router();
+
+const flatMapToValues = _.flatMap(({ data }) => _.values(data));
 
 router.get('/airtable', getCoins);
 
 router.get('/cmc', async (req, res) => {
-  const { data }: ICoinMarketCapResponse = await cmc.getTicker();
-  res.json(_.values(data));
+  const responses: ICoinMarketCapResponse[] = await Promise.all(
+    _.times(_.multiply(100), 15).map(start => cmc.getTicker({ start }))
+  );
+
+  const cmcCoins: ICoinMarketCapCoin[] = flatMapToValues(responses);
+
+  res.json(cmcCoins);
 });
 
 router.get('/*', (req, res) => res.sendFile(indexHtml));
